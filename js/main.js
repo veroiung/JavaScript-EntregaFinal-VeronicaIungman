eded730d6fe185d977d01958dc70c0fe1b43154d
veroiung <dg@veronicaiungman.com.ar>
2023-03-15 16:26:02 -0300

    DOM&LocalStorage

--- js/main.js
+++ js/main.js
@@ -1,208 +1,125 @@
-function Producto(nombre, precio) {
+
+function Producto(id, nombre, img, precio) {
+    this.id = id;
     this.nombre = nombre;
+    this.img = img;
     this.precio = precio;
 
     this.imprimirProducto = function () {
-        return "Este es el modeo " + (this.nombre) + " su precio es "(this.precio);
+        return "Esta es la identificacion" + (this.id) + "Este es el modelo " + (this.nombre) +  "La imagen " + (this.img) + " su precio es "(this.precio);
     }
+
 }
 let productos = []
-productos.push(new Producto("Denim Celeste", 4700))
-productos.push(new Producto("Denim Azul", 4700))
-productos.push(new Producto("Denim Blanco", 4700))
-productos.push(new Producto("Denim Negro", 4700))
-productos.push(new Producto("Azul Marino", 3900))
-productos.push(new Producto("Borravino", 3900))
-productos.push(new Producto("Skinny", 3900))
-productos.push(new Producto("Oliva", 3900))
-productos.push(new Producto("Mel Medio", 4800))
-productos.push(new Producto("Mel Claro", 4800))
-productos.push(new Producto("Mel Crudo", 4800))
-productos.push(new Producto("Negro Viscosa", 5200))
-productos.push(new Producto("Negro Flame", 5200))
-productos.push(new Producto("Petroleo", 3900))
-productos.push(new Producto("Nevado", 4500))
-productos.push(new Producto("Topo", 4500))
+productos.push(new Producto("denimceleste", "Denim Celeste", "./images/tienda/denim-celeste.jpg", 4700))
+productos.push(new Producto("denimazul", "Denim Azul", "./images/tienda/denim-azul.jpg", 4700))
+productos.push(new Producto("denimblanco", "Denim Blanco", "./images/tienda/denim-blanco.jpg", 4700))
+productos.push(new Producto("denimnegro", "Denim Negro", "./images/tienda/denim-negro.jpg", 4700))
+productos.push(new Producto("azulmarino", "Azul Marino", "./images/tienda/azul-marino.jpg", 3900))
+productos.push(new Producto("borravino", "Borravino", "./images/tienda/borravino.jpg", 3900))
+productos.push(new Producto("skinny", "Skinny", "./images/tienda/skinny.jpg", 3900))
+productos.push(new Producto("oliva", "Oliva", "./images/tienda/oliva.jpg", 3900))
+productos.push(new Producto("melmedio", "Mel Medio", "./images/tienda/mel-medio.jpg", 4800))
+productos.push(new Producto("melclaro", "Mel Claro", "./images/tienda/mel-claro.jpg", 4800))
+productos.push(new Producto("melcrudo", "Mel Crudo", "./images/tienda/mel-crudo.jpg", 4800))
+productos.push(new Producto("negroviscosa", "Negro Viscosa", "./images/tienda/negro-viscosa.jpg", 5200))
+productos.push(new Producto("negroflame", "Negro Flame", "./images/tienda/negro-flame.jpg", 5200))
+productos.push(new Producto("petroleo", "Petroleo", "./images/tienda/petroleo.jpg", 3900))
+productos.push(new Producto("nevado", "Nevado", "./images/tienda/nevado.jpg", 4500))
+productos.push(new Producto("topo", "Topo", "./images/tienda/topo.jpg", 4500))
 
+// const tienda = document.querySelector("#tienda"); //
+const tienda = document.getElementById("tienda");
+let botonesAgregar = document.querySelectorAll(".btn-agregar")
 
-let carrito = []
-let total = 0
+cargarProductos();
 
-let filtrado = prompt("Hola, desea ver los productos de menor precio, ¿si o no?");
+let numerito = document.getElementById("numerito");
 
-while (filtrado != "si" && filtrado != "no") {
-    alert("Por favor ingresa si o no");
-    filtrado = prompt("Desea ver los productos de menor precio, ¿si o no?");
-}
-if (filtrado == "si") {
+function cargarProductos() {
+    productos.forEach(producto => {
 
-    let min = Infinity;
-    let minimoPrecio = []
-    for (let i = 0; i < productos.length; i++) {
-        if (productos[i].precio == min){
-            minimoPrecio.push(productos[i]);
-            
-        } else if (productos[i].precio < min){
-            minimoPrecio = [];
-            minimoPrecio.push(productos[i]);
-            min = productos[i].precio
-           
-        }
-    }
-    alert("A continuacion nuestra lista de productos de menor precio.");
-    let productosString = "";
-    for (let i = 0; i < minimoPrecio.length; i++) {
-        productosString += minimoPrecio[i].nombre + " $" + minimoPrecio[i].precio + "\n";
-    }
-    minimoPrecio.splice(-1);
-    alert(productosString);
+        const div = document.createElement("div");
+        div.innerHTML = `
+                <div class="col mb-3">
+                    <div class="card" id="${producto.id}"">
+                            <img class="producto-imagen" src="${producto.img}" alt="${producto.nombre}">
+                        <div class="card-body">
+                            <h4 class="card-title"><strong>${producto.nombre}</strong></h4>
+                            <h5 class="card-title">$ ${producto.precio}</h5>
+                            <button type="button" class="btn-agregar" id="${producto.id}">+ Agregar al Carrito</button>
+                        </div>
+                    </div>
+                </div>
+        `;
+
+        tienda.append(div);
+    })
+    actualizarBotonesAgregar();
+  
 }
 
-    let seleccion = prompt("Hola, desea comprar algun producto, ¿si o no?")
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll(".btn-agregar");
 
-    while (seleccion != "si" && seleccion != "no") {
-        alert("Por favor ingresa si o no")
-        seleccion = prompt("Hola desea comprar algo ¿si o no?")
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+;
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+
+    actualizarNumerito();
+
+} else {
+productosEnCarrito = [];
+}
+
+
+function agregarAlCarrito(e){
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+         productosEnCarrito.push(productoAgregado);
+
     }
+    actualizarNumerito();
+    
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
 
-    if (seleccion == "si") {
-        alert("A continuacion nuestra lista de productos.")
-        let productosString = "";
-        for (let i = 0; i < productos.length; i++) {
-            productosString += productos[i].nombre + " $" + productos[i].precio + "\n";
-        }
-        productos.splice(-1);
-        alert(productosString);
-    } else if (seleccion == "no") {
-        alert("¡Gracias por visitarnos, hasta pronto!")
+    function actualizarNumerito(){
+        let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+        numerito.innerText = nuevoNumerito;
 
-    }
+}
 
-    while (seleccion != "no") {
-        let precio = 0
-        let notFound = true;
-        while (notFound) {
-            let producto = prompt("Agrega un producto a tu carrito")
-            switch (producto.toLowerCase()) {
 
-                case "denim celeste":
-                    precio = 4700
-                    notFound = false;
-                    break
-                case "denim azul":
-                    precio = 4700
-                    notFound = false;
-                    break
-                case "denim blanco":
-                    precio = 4700
-                    notFound = false;
-                    break
-                case "denim negro":
-                    precio = 4700
-                    notFound = false;
-                    break
-                case "azul marino":
-                    precio = 3900
-                    notFound = false;
-                    break
-                case "borravino":
-                    precio = 3900
-                    notFound = false;
-                    break
-                case "skinny":
-                    precio = 3900
-                    notFound = false;
-                    break
-                case "oliva":
-                    precio = 3900
-                    notFound = false;
-                    break
-                case "mel medio":
-                    precio = 4800
-                    notFound = false;
-                    break
-                case "mel claro":
-                    precio = 4800
-                    notFound = false;
-                    break
-                case "mel crudo":
-                    precio = 4800
-                    notFound = false;
-                    break
-                case "negro viscosa":
-                    precio = 5200
-                    notFound = false;
-                    break
-                case "negro flame":
-                    precio = 5200
-                    notFound = false;
-                    break
-                case "petroleo":
-                    precio = 3900
-                    notFound = false;
-                    break
-                case "nevado":
-                    precio = 4500
-                    notFound = false;
-                    break
-                case "topo":
-                    precio = 4500
-                    notFound = false;
-                    break
-                default:
-                    notFound = true;
-                    alert("No tenemos ese producto")
-            }
 
-            if (notFound) {
-                break;
-            }
 
-            let talle = Number(prompt("¿Qué talle desea?\n¿1, 2, 3, 4 o 5?"))
 
-            while (!Number.isInteger(talle) || talle < 1 || talle > 5) {
-                alert("Por favor ingresa un talle entre 1 y 5")
-                talle = Number(prompt("¿Qué talle desea?\n¿1, 2, 3, 4 o 5?"))
-            }
 
-            let unidades = Number(prompt("¿Cuántas unidades desea?"))
 
-            while (!Number.isInteger(unidades) || unidades < 1) {
-                alert("No es un valor válido. Por favor ingresa la cantidad ")
-                unidades = Number(prompt("¿Cuántas unidades desea?"))
-            }
-
-            carrito.push({
-                producto,
-                talle,
-                unidades,
-                precio
-            })
-            total += precio * unidades;
-            console.log(carrito)
-
-            seleccion = prompt("¿Desea seguir comprando?")
-
-            while (seleccion === "no") {
-                alert("¡Gracias por la compra, hasta pronto!")
-                carrito.forEach((item) => {
-                    console.log(`producto: ${capitalize(item.producto)}, talle: ${item.talle}, unidades: ${item.unidades}, total a pagar por producto ${item.unidades * item.precio}`)
-                })
-                break;
-            }
-
+/*
+function agregar(identificador) {
+    for (let i=0; i<productos.length; i++) {
+        if(identificador == productos[i].id){
+            console.log(productos[i].nombre, productos[i].precio); 
+            break;
         }
     }
+}*/
 
-    console.log(`El total a pagar por su compra es: ${total}`)
-
-    function capitalize(str) {
-        const lowerStr = str.toLowerCase();
-        const words = lowerStr.split(" ");
-
-        for (let i = 0; i < words.length; i++) {
-            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-        }
-
-        return words.join(" ");
-    }
