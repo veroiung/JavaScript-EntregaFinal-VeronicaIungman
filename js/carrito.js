cb88bead40513fbbda52b4c8210af2958743c94a
veroiung <dg@veronicaiungman.com.ar>
2023-03-29 15:09:42 -0300

    una S

--- js/carrito.js
+++ js/carrito.js
@@ -146,8 +146,8 @@ botonComprar.addEventListener("click", comprarCarrito);
 function comprarCarrito(){
 
     productosEnCarrito.length = 0;
-    localStorage.setItem("producto-en-carrito", JSON.stringify(productosEnCarrito));
-        
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+   
     contenedorCarritoVacio.classList.add("disabled");
     contenedorCarritoProductos.classList.add("disabled");
     contenedorCarritoAcciones.classList.add("disabled");
